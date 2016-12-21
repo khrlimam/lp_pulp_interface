385d860cde5c58373edf5c2be1e8d039dc2d1b91
Khairul Imam <ki65559@gmail.com>
2016-12-22 02:47:49 +0800

    quick and dirty finish

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import (
     jsonify,
     render_template,
     request)
+from lp.solver import Solver as simplex
+import pulp
+from lp.NoSolutionException import NoSolutionException
 
 app = Flask("Python Simplex")
 
@@ -14,14 +17,33 @@ def index():
 
 @app.route("/process", methods=['POST'])
 def process():
-    data = {
-        'title': request.form['title'],
-        'constraints': request.form['constraints'],
-        'objective': request.form['objective'],
-        'variables': request.form['variables'],
-        'z': request.form['z']
-    };
-    return jsonify(data)
+    # dirty code and magic happens here
+    # what important for now the code works, so i got A grade 3:)
+    variables = request.form['variables'].split(',')
+    constraints = request.form['constraints'].split(',')
+    objective = request.form['objective']
+    title = request.form['title']
+    z = int(request.form['z'])
+    what_to_achieve = (pulp.LpMinimize, pulp.LpMaximize)[z]
+    # apply input as lp variable
+    for var in variables:
+        exec ("{} = pulp.LpVariable('{}', lowBound=0, cat=pulp.LpInteger)".format(var, var))
+    holy_problem = simplex(title, what_to_achieve)
+    holy_problem.constraints(list=[eval(constraint) for constraint in constraints])
+    holy_problem.objective(eval(objective))
+    try:
+        holy_problem.any_solution()
+        response = {
+            'varsAndSolution': [{'name': var.name, 'val': var.value()} for var in holy_problem.__prob__.variables()],
+            'z': holy_problem.__prob__.objective.value(),
+            'execTime': holy_problem.__prob__.solutionTime
+        }
+    except NoSolutionException:
+        response = {
+            'error': True,
+            'message': NoSolutionException.message
+        }
+    return jsonify(response)
 
 
 @app.route("/help")
--- lp/solver.py
+++ lp/solver.py
@@ -1,4 +1,24 @@
 import pulp
+from NoSolutionException import NoSolutionException
+
 
 class Solver:
-    def __init__(self):
+    def __init__(self, problem_name, what_to_do):
+        self.problem_name = problem_name
+        self.__prob__ = pulp.LpProblem(self.problem_name, what_to_do)
+
+    def constraints(self, list=None, *fks):
+        if list is not None:
+            fks = list
+        for val in fks:
+            self.__prob__ += val
+
+    def objective(self, ft):
+        self.__prob__ += ft
+
+    def any_solution(self):
+        result = self.__prob__.solve()
+        try:
+            assert result == pulp.LpStatusOptimal
+        except AssertionError:
+            raise NoSolutionException("Tidak ada solusi!")
