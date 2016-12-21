dd76b3477ae862adbd6a9b40ac3a9e23d79d8ff6
Khairul Imam <ki65559@gmail.com>
2016-12-21 22:50:34 +0000

    merge forgotten conflict line

--- server.py
+++ server.py
@@ -3,8 +3,4 @@ import os
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 9090)
-<<<<<<< HEAD
     app.run(host='0.0.0.0', port=port)
-=======
-    python_simplex.run(host='0.0.0.0', port=port)
->>>>>>> c3bde74c0426b99c418ea571ae54865fc21e49bd
